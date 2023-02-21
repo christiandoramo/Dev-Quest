82580087e3036967043997a70e725e26f4ec5013
christiandoramo <christiandoramo@gmail.com>
2023-02-21 14:38:18 -0300

    inicio css avançado parte 2

---
+++ cssAvancado2/src/js/align-items.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body')
+const directions = ['row', 'column']
+const orgs = ['flex-start', 'flex-end', 'center', 'stretch', 'baseline']
+directions.forEach((direction) => {
+  orgs.forEach((org) => {
+    let section = document.createElement('section')
+    section.innerHTML = `
+        <section>
+            <h2>${org} <br> flex-direction: ${direction}</h2>
+            <div class="container ${org} ${direction}">
+            <div class="item item1">O ITEM 1</div>
+            <div class="item item2">O ITEM 2</div>
+            <div class="item item3">O ITEM 3</div>
+            </div>
+        </section>
+        `
+    body.appendChild(section)
+  })
+})
