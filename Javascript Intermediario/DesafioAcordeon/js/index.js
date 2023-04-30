ea4582ea660aa23ba64ed0c46a995c0367e42110
christiandoramo <christiandoramo@gmail.com>
2023-04-29 23:38:21 -0300

    desafio acordeon

---
+++ Javascript Intermediario/DesafioAcordeon/js/index.js
@@ -0,0 +1,11 @@
+/*
+  ao clicar em uma pergunta deve acontecer
+uma animação de acordeon para abrir o item
+e mostrar o texto escondido, e a seta deve
+mudar de cor.
+ao clicar em um item para abri-lo todos os
+outros itens devem ser fechados.
+Referência do que é um efeito acordeon
+https://www.netflix.com/br/ na seção
+"Perguntas Frequentes".
+*/
